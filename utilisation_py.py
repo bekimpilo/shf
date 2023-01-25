a46ee0b081d889840b676ff0b3a8bc02dec94547
bekimpilo <50261487+bekimpilo@users.noreply.github.com>
2023-01-25 23:46:48 +0200

    Update utilisation_py.py

--- utilisation_py.py
+++ utilisation_py.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_title="CSV Appender", page_icon=":guardsman:", layout="wide")
 st.image("chailogo.png", width=200)
-st.title("Top 20% medicine procured by NDoH contributing 80% annual spend)
+st.title("Top 20% medicine procured by NDoH contributing 80% annual spend")
 
 
 file_1 = st.file_uploader("Upload the first CSV file", type=["csv"])
