981b473f5e5de841014d01727b77a1d20d7083ef
bekimpilo <50261487+bekimpilo@users.noreply.github.com>
2023-01-26 13:23:41 +0200

    Update utilisation_py.py

--- utilisation_py.py
+++ utilisation_py.py
@@ -23,7 +23,7 @@ if st.button('Append'):
     st.success('Appending the two dataset...')
     df_appended = pd.concat([df1, df2], axis=0)
     st.dataframe(df_appended.head())
-    df_appended["pct_contribution"] = df_appended.groupby("nsn")["order_qty"].transform(lambda x: x/x.sum()*100)
+    df_appended["pct_contribution"] = df_appended.groupby("description")["order_qty"].transform(lambda x: x/x.sum()*100)
     df_appended_sorted = df_appended.sort_values(by="pct_contribution", ascending=False)
     df_appended_sorted["cumulative_pct"] = df_appended_sorted["pct_contribution"].cumsum()
     important_medicines = df_appended_sorted[df_appended_sorted["cumulative_pct"] <= 80]
