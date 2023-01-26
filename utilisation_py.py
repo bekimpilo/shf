60672277cf89e6e8c75a66ad929031e254b909a6
bekimpilo <50261487+bekimpilo@users.noreply.github.com>
2023-01-26 13:10:34 +0200

    Update utilisation_py.py

--- utilisation_py.py
+++ utilisation_py.py
@@ -18,8 +18,10 @@ if file_1:
         df2 = pd.read_csv(file_2)
         st.subheader("The first 5 rows of dataset 2", color="blue")
         st.dataframe(df2.head())
-
-    if st.button('Append'):
+else:
+    st.warning("No file was uploaded.")
+    
+     if st.button('Append'):
         st.success('Appending the two dataset...')
         df_appended = pd.concat([df1, df2], axis=0)
         st.subheader("The first 5 rows of the appended dataset", color="blue")
@@ -29,8 +31,6 @@ if file_1:
         df_appended_sorted["cumulative_pct"] = df_appended_sorted["pct_contribution"].cumsum()
         important_medicines = df_appended_sorted[df_appended_sorted["cumulative_pct"] <= 80]
         st.dataframe(important_medicines)
-else:
-    st.warning("No file was uploaded.")
    
     if st.button('Download CSV'):
         important_medicines.to_csv("important_medicines.csv", index=False)
